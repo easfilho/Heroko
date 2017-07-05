90ee7eb9a21e16f1e19d3e9bf3d094f809789506
Edson Andrade <edson_andrade_filho@hotmail.com>
2017-07-05 18:05:51 -0300

    commit travis

---
+++ Heroko/Models/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace Heroko.Models
+{
+    public class Calculadora
+    {
+        private int Numero1;
+        private int Numero2;
+
+        public Calculadora(int Numero1, int Numero2)
+        {
+            this.Numero1 = Numero1;
+            this.Numero2 = Numero2;
+        }
+
+        public int Somar()
+        {
+            return Numero1 + Numero2;
+        }
+
+    }
+}
---
+++ Heroko/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Heroko.Startup))]
+namespace Heroko
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
